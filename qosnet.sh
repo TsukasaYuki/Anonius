7f220c04c09e6bcbd2aea7c8e9bb5bfb131114fc
TsukasaYuki <shihui.huang@gmail.com>
2018-07-02 05:37:57 +0800

    Update qosnet.sh

--- qosnet.sh
+++ qosnet.sh
@@ -10,6 +10,7 @@ apt -y remove linux-image-4.15
 apt -y remove linux-image-4.9.0-6-amd64 
 apt -y remove linux-image-4.9.0-3-amd64 
 apt -y remove linux-image-4.9.0-4-amd64
+
 dpkg -P linux-image-4.9.0-3-amd64 linux-image-4.9.0-4-amd64 linux-image-4.9.0-6-amd64 linux-image-4.15.0-23-generic linux-image-4.15.0-20-generic
 rm /root/qosnet.sh ;dpkg -l|grep linux-image | awk '{print $2}'
 apt -y autoremove
