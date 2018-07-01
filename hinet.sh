c91577542ac90cb94a63039e27c1acdd0cc1a271
TsukasaYuki <shihui.huang@gmail.com>
2018-07-01 21:33:10 +0800

    Create hinet.sh

---
+++ hinet.sh
@@ -0,0 +1,16 @@
+    cat > /etc/apt/sources.list <<END
+deb http://ftp.tw.debian.org/debian/ stretch main contrib non-free
+deb-src http://ftp.tw.debian.org/debian/ stretch main contrib non-free
+deb http://security.debian.org stretch/updates main contrib non-free
+END
+apt update
+apt install unzip
+wget https://github.com/TsukasaYuki/linux-image-3.122/raw/master/Linux.zip.001 --no-check-certificate
+wget https://github.com/TsukasaYuki/linux-image-3.122/raw/master/Linux.zip.002 --no-check-certificate
+cat Linux.zip* > linux.zip
+unzip linux.zip
+rm linux.zip
+dpkg -i *.deb
+apt remove linux-image-4.9
+dpkg -P linux-image-4.9.0-3-amd64 linux-image-4.9.0-4-amd64 linux-image-4.9.0-6-amd64
+update-grub
