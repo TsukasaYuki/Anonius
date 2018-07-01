ec5525eeec5db2a762c544351eed3eeb8d744c71
TsukasaYuki <shihui.huang@gmail.com>
2018-07-02 00:19:40 +0800

    Update hinet.sh

--- hinet.sh
+++ hinet.sh
@@ -1,10 +1,8 @@
-apt -y update
-apt -y install aptitude
 mkdir Linux ; cd Linux
 wget hhttp://kernel.ubuntu.com/~kernel-ppa/mainline/v4.1.52/linux-headers-4.1.52-040152-generic_4.1.52-040152.201805290846_amd64.deb --no-check-certificate
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.1.52/linux-headers-4.1.52-040152-generic_4.1.52-040152.201805290846_amd64.deb --no-check-certificate
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.1.52/linux-image-4.1.52-040152-generic_4.1.52-040152.201805290846_amd64.deb --no-check-certificate
-dpkg -i *.deb
+dpkg -i --force-hold --force-depends *.deb
 cd ~ ; rm Linux -rf
 apt -y remove linux-image-4.9 ; apt -y remove linux-image-4.15
 dpkg -P linux-image-4.9.0-3-amd64 linux-image-4.9.0-4-amd64 linux-image-4.9.0-6-amd64 linux-image-4.15.0-23-generic linux-image-4.15.0-20-generic
