2701bd63c94e5ee5b71558c6a175424f7d5988f5
TsukasaYuki <shihui.huang@gmail.com>
2018-07-01 22:00:45 +0800

    Update hinet.sh

--- hinet.sh
+++ hinet.sh
@@ -16,6 +16,7 @@ dpkg -i *.deb
 cd ~
 rm Linux -rf
 apt -y -f remove linux-image-4.9
+dpkg --get-selections | grep linux-image
 dpkg -P linux-image-4.9.0-3-amd64 linux-image-4.9.0-4-amd64 linux-image-4.9.0-6-amd64
 update-grub
 rm /root/hinet.sh
