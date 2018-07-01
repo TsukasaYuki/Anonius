93182efdb0ca53664ab5c14e14795d5560a43129
TsukasaYuki <shihui.huang@gmail.com>
2018-07-02 00:40:27 +0800

    Update hinet.sh

--- hinet.sh
+++ hinet.sh
@@ -5,7 +5,7 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.1.52/linux-image-4.1.52-04
 dpkg -i --force-hold --force-depends *.deb
 cd ~ ; rm Linux -rf
 dpkg -P linux-image-4.9.0-3-amd64 linux-image-4.9.0-4-amd64 linux-image-4.9.0-6-amd64 linux-image-4.15.0-23-generic linux-image-4.15.0-20-generic
-apt -y remove linux-image-4.9 linux-image-4.15
+apt -y remove linux-image-4.9 linux-image-4.15 linux-image-4.9.0-6-amd64 linux-image-4.9.0-3-amd64 linux-image-4.9.0-4-amd64
 apt -y --fix-broken install
 update-grub
 rm /root/hinet.sh ;dpkg -l|grep linux-image | awk '{print $2}'
