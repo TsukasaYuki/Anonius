39194ae3d6dbed61398f50de82f4f7dc8c4c5785
TsukasaYuki <shihui.huang@gmail.com>
2018-07-01 22:44:07 +0800

    Update hinet.sh

--- hinet.sh
+++ hinet.sh
@@ -14,6 +14,9 @@ apt -y -f remove linux-image-4.9
 apt -y -f remove linux-image-4.15
 dpkg --get-selections | grep linux-image
 dpkg -P linux-image-4.9.0-3-amd64 linux-image-4.9.0-4-amd64 linux-image-4.9.0-6-amd64
-echo "linux-image-3.12-1-amd64 hold" | dpkg --set-selections
+#echo "linux-image-3.12-1-amd64 hold" | dpkg --set-selections
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.18.113/linux-headers-3.18.113-0318113-generic_3.18.113-0318113.201806131532_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.18.113/linux-image-3.18.113-0318113-generic_3.18.113-0318113.201806131532_amd64.deb
+dpkg -i --force-hold --force-depends *.deb
 update-grub
 rm /root/hinet.sh
