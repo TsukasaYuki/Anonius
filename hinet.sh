096f407c5447945cd7fef51a19dc5f8946beb976
TsukasaYuki <shihui.huang@gmail.com>
2018-07-01 21:38:29 +0800

    Update hinet.sh

--- hinet.sh
+++ hinet.sh
@@ -5,12 +5,16 @@ deb http://security.debian.org stretch/updates main contrib non-free
 END
 apt update
 apt install unzip
+mkdir Linux
+cd Linux
 wget https://github.com/TsukasaYuki/linux-image-3.122/raw/master/Linux.zip.001 --no-check-certificate
 wget https://github.com/TsukasaYuki/linux-image-3.122/raw/master/Linux.zip.002 --no-check-certificate
 cat Linux.zip* > linux.zip
 unzip linux.zip
 rm linux.zip
 dpkg -i *.deb
-apt remove linux-image-4.9
+cd ~
+rm Linux -rf
+apt -y -f remove linux-image-4.9
 dpkg -P linux-image-4.9.0-3-amd64 linux-image-4.9.0-4-amd64 linux-image-4.9.0-6-amd64
 update-grub
